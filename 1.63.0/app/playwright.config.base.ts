e9e889e583ca11abdc8a18ca9bc9ae7fb81afc66
janejulie <jene@mail.de>
2026-09-07 15:58:55 +0200

    feat: adding 1.63.0

---
+++ 1.63.0/app/playwright.config.base.ts
@@ -0,0 +1,81 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: process.env.TEST_ROOT_DIR,
+  outputDir: './test-results',
+  // testIgnore: '*test-assets',
+  testMatch: '**/*.spec.ts',
+  snapshotPathTemplate: '{testDir}/test-snapshots/{testFilePath}/{testName}/{projectName}-{platform}{ext}',
+  fullyParallel: true,
+  forbidOnly: true,
+  retries: 0,
+  workers: 10,
+  reporter: [
+    ['list'],
+    ['html', { open: 'never', outputFolder: './test-reports' }],
+    ['json', { outputFile: './test-reports/results.json' }],
+    ['junit', { outputFile: './test-reports/results.xml' }],
+  ],
+  use: {
+    baseURL: process.env.TEST_BASE_URL,
+    trace: 'on-first-retry',
+    ignoreHTTPSErrors: true,
+  },
+  projects: [
+    {
+      name: 'chromium-xs',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 480, height: 700 },
+      },
+    },
+    {
+      name: 'chromium-md',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 700, height: 1000 },
+      },
+    },
+    {
+      name: 'chromium-lg',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1024, height: 768 },
+      },
+    },
+    {
+      name: 'chromium-xl',
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    //
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+});
